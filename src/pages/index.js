a09a41917c6c76fbfad531acf8e69d844e5ab2d7
cynthialdouglas <cynthia.l.douglas@gmail.com>
2020-11-05 10:44:30 -0500

    Updated with Babel Styled Components and src files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from "styled-components"
+
+import TicTacToe from "../components/TicTacToe"
+
+const GameBody = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <SEO title="Game" />
+    <GameBody>
+      <TicTacToe />
+    </GameBody>
   </Layout>
 )
 
